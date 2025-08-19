49805f7ab6347195bf30cb29e4ce5bdeffeb67dd
SaifAlqady51 <saifalqady51@gmail.com>
2025-08-19 22:08:12 +0300

    refactor: Updated mobile app

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -8,6 +8,8 @@ export default function Index() {
         justifyContent: "center",
         alignItems: "center",
       }}
-    ></View>
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
   );
 }
