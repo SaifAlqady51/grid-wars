9ba68fc13fd60afc50aae7c968c309f48dc7d3f7
SaifAlqady51 <saifalqady51@gmail.com>
2025-08-31 21:27:59 +0300

    feat: Added validators filters in main.ts file

--- apps/backend/services/account-service/src/main.ts
+++ apps/backend/services/account-service/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
-import { HttpExceptionFilter } from './filter/http-exception.filter';
-import { AppModule } from './account.module';
+import { HttpExceptionFilter, ValidationExceptionFilter } from '@/filter';
+import { AccountModule } from './account.module';
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new HttpExceptionFilter());
+  const app = await NestFactory.create(AccountModule);
+  app.useGlobalFilters(
+    new HttpExceptionFilter(),
+    new ValidationExceptionFilter(),
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        return new BadRequestException(errors);
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
