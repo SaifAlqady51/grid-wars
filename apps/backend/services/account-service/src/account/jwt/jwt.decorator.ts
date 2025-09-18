fbbddaee4168b31ba56357e5e3d2c16641e1c75e
SaifAlqady51 <saifalqady51@gmail.com>
2025-09-18 21:39:03 +0300

    delete: Deleted jwt from account-service

--- apps/backend/services/account-service/src/account/jwt/jwt.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { UserPayload } from './jwt.types';
-
-export const JwtPayload = createParamDecorator(
-  (_, context: ExecutionContext): UserPayload => {
-    const request = context.switchToHttp().getRequest();
-    return request.user;
-  },
-);
