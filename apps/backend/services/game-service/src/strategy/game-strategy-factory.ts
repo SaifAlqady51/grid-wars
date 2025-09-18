81a65e017a5adb56f514b2c343d8973c5a4518b2
SaifAlqady51 <saifalqady51@gmail.com>
2025-09-18 21:39:54 +0300

    feat: created create game strategy for game-servcie

---
+++ apps/backend/services/game-service/src/strategy/game-strategy-factory.ts
@@ -0,0 +1,17 @@
+import { GameMode } from '@/entity';
+import { GameStrategy } from './game-strategy';
+import { AiGameStrategy } from './ai-game-strategy';
+import { PVPGameStrategy } from './pvp-game-strategy';
+
+export class GameStrategyFactory {
+  static getStrategy(gameMode: GameMode): GameStrategy {
+    switch (gameMode) {
+      case GameMode.AI:
+        return new AiGameStrategy();
+      case GameMode.PVP:
+        return new PVPGameStrategy();
+      default:
+        throw new Error(`Unsupported game mode: ${gameMode}`);
+    }
+  }
+}
